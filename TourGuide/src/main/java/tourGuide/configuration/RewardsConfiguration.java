06402202646bb910b197522b2b1ce3dc026de803
toastnjamz <veritydavis@gmail.com>
2020-12-14 12:46:46 -0700

    Removin unused dependencies

--- TourGuide/src/main/java/tourGuide/configuration/RewardsConfiguration.java
+++ TourGuide/src/main/java/tourGuide/configuration/RewardsConfiguration.java
@@ -1,10 +1,8 @@
 package tourGuide.configuration;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.web.client.RestTemplate;
 import rewardCentral.RewardCentral;
 import tripPricer.TripPricer;
 
